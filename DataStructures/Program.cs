910d64616981a271d88d3a8351e16c1b0a1b53f9
Kamran Sadin <mrsadin@gmail.com>
2022-08-07 21:55:15 +0430

    Array-01

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,7 +6,11 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data Strucures\n\n\n");
+            var array1 = IntList.Of(2,3,4,7,345);
+
+            Console.WriteLine(array1.ToString());
+
+            Console.ReadLine();
         }
     }
 }
