d6998952408954bbc21847123fa8916d806838f5
Kamran Sadin <mrsadin@gmail.com>
2022-08-27 22:13:04 +0430

    Adding LinkedList data structure

---
+++ DataStructures/Adts/MyLinkedList.cs
@@ -0,0 +1,134 @@
+﻿using System;
+namespace DataStructures.Adts
+{
+	public class MyLinkedList<T>
+	{
+        private Node<T> _head = null;
+        private Node<T> _tail = null;
+        private int _size = 0;
+
+        public int Size()
+        {
+            return _size;
+        }
+
+        public void AddToStart(T value)
+        {
+            this._head = new Node<T>(value, this._head);
+            if (this.IsEmpty())
+            {
+                this._tail = this._head;
+            }
+            this._size++;
+        }
+
+        public void AddToEnd(T value)
+        {
+            var node = new Node<T>(value, null);
+            if (this.IsEmpty())
+            {
+                this._head = node;
+            }
+            else
+            {
+                this._tail.SetNext(node);
+            }
+            this._tail = node;
+            this._size++;
+        }
+
+        public T Get(int i)
+        {
+            if (IsEmpty() || i < 0)
+            {
+                throw new Exception("Not such element found.");
+            }
+
+            if (i == 0)
+                return this._head.GetValue();
+
+            if (i == _size - 1)
+                return this._tail.GetValue();
+
+            Node<T> current = this._head;
+            int index = 0;
+
+            while (index != i)
+            {
+                current = current.GetNext();
+                index++;
+                if (current == null)
+                {
+                    throw new Exception("Not such element found.");
+                }
+            }
+
+            return current.GetValue();
+        }
+
+        public void Remove(int index)
+        {
+            if (this.IsEmpty())
+                throw new Exception("No such element found.");
+
+            var prev = _head;
+            var current = _head.GetNext();
+
+            for (var i = 1; i < index; i++)
+            {
+                if (current == null)
+                    throw new Exception("No such element found");
+                prev = current;
+                current = current.GetNext();
+            }
+            prev.SetNext(current.GetNext());
+            current.SetNext(null);
+        }
+
+        // TODO: Implementing Insert
+
+
+
+
+
+        private bool IsEmpty()
+        {
+            return this._size == 0;
+        }
+
+
+
+        private class Node<T>
+        {
+            private T _value;
+            private Node<T> _next;
+
+            public Node(T value, Node<T> next)
+            {
+                this._value = value;
+                this._next = next;
+            }
+
+            public T GetValue()
+            {
+                return this._value;
+            }
+
+            public Node<T> GetNext()
+            {
+                return this._next;
+            }
+
+            public void SetValue(T value)
+            {
+                this._value = value;
+            }
+
+            public void SetNext(Node<T> next)
+            {
+                this._next = next;
+            }
+        }
+    }
+}
+
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -29,18 +29,38 @@ namespace DataStructures
 
             //Console.WriteLine(queue.ToString());
 
-            //  24 - 33 - 67 - 12
-            var myStack = new MyStack<int>();
-            myStack.Push(24);
-            myStack.Push(33);
-            myStack.Push(67);
-            myStack.Push(12);
+            ////  24 - 33 - 67 - 12
+            //var myStack = new MyStack<int>();
+            //myStack.Push(24);
+            //myStack.Push(33);
+            //myStack.Push(67);
+            //myStack.Push(12);
 
-            Console.WriteLine(myStack.ToString());
-            Console.WriteLine(myStack.Pop());
-            Console.WriteLine(myStack.ToString());
+            //Console.WriteLine(myStack.ToString());
+            //Console.WriteLine(myStack.Pop());
+            //Console.WriteLine(myStack.ToString());
 
-            Console.WriteLine(myStack.Top());
+            //Console.WriteLine(myStack.Top());
+
+
+            // 2 - 48 - 102 - 200
+            var list = new MyLinkedList<int>();
+            list.AddToEnd(2);
+            list.AddToEnd(48);
+            list.AddToEnd(102);
+            list.AddToEnd(200);
+
+            //Console.WriteLine(list.Get(1));
+            Console.WriteLine(list.Get(3));
+            //Console.WriteLine(list.Get(4)); // Throws and exception.
+            //Console.WriteLine(list.Get(-1)); // Throws and exception.
+
+
+            //list.Remove(1);
+            //Console.WriteLine(list.Get(0));
+            //Console.WriteLine(list.Get(1));
+            //Console.WriteLine(list.Get(2));
+
 
 
 
