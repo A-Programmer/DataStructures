bfd9dab90ce1fdedf05e847da667897c817fe0af
Kamran Sadin <mrsadin@gmail.com>
2022-09-03 21:05:01 +0430

    Selection Sort

---
+++ DataStructures/Algorithms/SelectionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace DataStructures.Algorithms
+{
+    public class SelectionSort
+    {
+        public int[] Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    int temp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = temp;
+                }
+            }
+
+            return array;
+        }
+    }
+}
+
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DataStructures.Adts;
+using DataStructures.Algorithms;
 
 namespace DataStructures
 {
@@ -7,6 +8,7 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            #region Dynamic Array (List)
             //var array = MyList<int>.Of(3, 6, 77);
             //Console.WriteLine(array.ToString());
             //Console.WriteLine(array.Get(1).ToString());
@@ -16,8 +18,9 @@ namespace DataStructures
             //Console.WriteLine(array.Insert(1, 22).Insert(2, 55).ToString());
 
             //Console.WriteLine(array.Remove(1).ToString());
+            #endregion
 
-
+            #region Stack and Queue
             //var queue = new MyQueue<int>();
             //queue.Add(24).Add(45).Add(33);
             //Console.WriteLine(queue.ToString());
@@ -41,17 +44,18 @@ namespace DataStructures
             //Console.WriteLine(myStack.ToString());
 
             //Console.WriteLine(myStack.Top());
+            #endregion
 
-
+            #region Linked List
             // 2 - 48 - 102 - 200
-            var list = new MyLinkedList<int>();
-            list.AddToEnd(2);
-            list.AddToEnd(48);
-            list.AddToEnd(102);
-            list.AddToEnd(200);
+            //var list = new MyLinkedList<int>();
+            //list.AddToEnd(2);
+            //list.AddToEnd(48);
+            //list.AddToEnd(102);
+            //list.AddToEnd(200);
 
             //Console.WriteLine(list.Get(1));
-            Console.WriteLine(list.Get(3));
+            //Console.WriteLine(list.Get(3));
             //Console.WriteLine(list.Get(4)); // Throws and exception.
             //Console.WriteLine(list.Get(-1)); // Throws and exception.
 
@@ -60,8 +64,29 @@ namespace DataStructures
             //Console.WriteLine(list.Get(0));
             //Console.WriteLine(list.Get(1));
             //Console.WriteLine(list.Get(2));
+            #endregion
 
+            #region Selection Sort
+            var array = new int[] { 10, 23, 5, 6, 3 };
+            var selectionSort = new SelectionSort();
+            var sortedArray = selectionSort.Sort(array);
 
+            Console.Write("{");
+            foreach (var item in array)
+            {
+                Console.Write($"{item},");
+            }
+            Console.Write("}");
+
+            Console.WriteLine("\n\n\n");
+
+            Console.Write("{");
+            foreach (var item in sortedArray)
+            {
+                Console.Write($"{item},");
+            }
+            Console.Write("}");
+            #endregion
 
 
 
